bd234e394778d38fd1f572ec331edc038b3c02af
Florian Reihl <59579743+Florian268@users.noreply.github.com>
2024-09-23 16:36:30 -0400

    Progress: Tree Walker Works Now

--- src/Layouts/createTreeWalker.jsx
+++ src/Layouts/createTreeWalker.jsx
@@ -1,4 +1,4 @@
-export default function createTreeWalker(rootNode, textToHighlight, highlightClassName, onMouseEnter, onMouseLeave) {
+export default function createTreeWalker(rootNode, textToHighlight, highlightClassName, definition) {
 	const treeWalker = document.createTreeWalker(
 		rootNode,
 		NodeFilter.SHOW_TEXT,
@@ -24,13 +24,53 @@ export default function createTreeWalker(rootNode, textToHighlight, highlightCla
 			const span = document.createElement('span');
 			span.className = highlightClassName;
 			span.textContent = highlightedText;
-			span.addEventListener('mouseenter', onMouseEnter, true); // useCapture set to true
-			span.addEventListener('mouseleave', onMouseLeave, true);
 
 			const { parentNode } = node;
 			parentNode.replaceChild(document.createTextNode(beforeText), node);
 			parentNode.insertBefore(span, node.nextSibling);
 			parentNode.insertBefore(document.createTextNode(afterText), span.nextSibling);
+
+			let tooltip;
+
+			span.addEventListener('mouseover', () => {
+				if (!tooltip) {
+					tooltip = document.createElement('div');
+					tooltip.innerHTML = `${definition} <a href="glossary#${textToHighlight}" class="text-blue-500 underline">Learn More</a>`; // Add the link
+					tooltip.style.position = 'absolute';
+					tooltip.style.backgroundColor = 'white';
+					tooltip.style.padding = '5px';
+					tooltip.style.border = '1px solid gray';
+					tooltip.style.borderRadius = '3px';
+
+					// Get the position of the span
+					const spanRect = span.getBoundingClientRect();
+					// Get position of the scroll
+					const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+					tooltip.style.left = `${spanRect.left}px`;
+					tooltip.style.top = `${spanRect.bottom + scrollTop}px`;
+
+					tooltip.addEventListener('mouseleave', () => {
+						hideTooltipAfterDelay();
+					});
+
+					span.addEventListener('mouseleave', () => {
+						hideTooltipAfterDelay();
+					});
+				}
+				document.body.appendChild(tooltip);
+				span.tooltip = tooltip;
+			});
+
+			function hideTooltipAfterDelay() {
+				setTimeout(() => {
+					if (span && span.tooltip && document.body.contains(span.tooltip)) {
+						if (!span.matches(':hover') && !span.tooltip.matches(':hover')) {
+							document.body.removeChild(span.tooltip);
+							span.tooltip = null;
+						}
+					}
+				}, 500);
+			}
 		}
 	}
 }
--- src/Layouts/layout.jsx
+++ src/Layouts/layout.jsx
@@ -3,58 +3,17 @@ import { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import createTreeWalker from './createTreeWalker';
-import Popup from './popup';
 import glossary from './glossary';
 
 const Layout = ({ children }) => {
-	const [popupData, setPopupData] = useState({ show: false, definition: '', position: { top: 0, left: 0 } });
-
 	useEffect(() => {
 		const observer = new MutationObserver(() => {
 			glossary.forEach(({ word, definition }) => {
-				createTreeWalker(
-					document.body,
-					word,
-					'highlight',
-					(event) => {
-						// onMouseEnter callback
-						setPopupData({
-							show: true,
-							definition,
-							position: {
-								top: event.clientY,
-								left: event.clientX,
-							},
-						});
-					},
-					() => {
-						// onMouseLeave callback
-						setPopupData({ ...popupData, show: false });
-					},
-				);
+				createTreeWalker(document.body, word, 'highlight', definition);
 				// child
 				const contentElement = document.querySelector('.px-5.md\\:px-0 > div:nth-of-type(2)');
 				if (contentElement) {
-					createTreeWalker(
-						contentElement,
-						word,
-						'highlight',
-						(event) => {
-							// onMouseEnter callback
-							setPopupData({
-								show: true,
-								definition,
-								position: {
-									top: event.clientY,
-									left: event.clientX,
-								},
-							});
-						},
-						() => {
-							// onMouseLeave callback
-							setPopupData({ ...popupData, show: false });
-						},
-					);
+					createTreeWalker(contentElement, word, 'highlight', definition);
 				}
 			});
 		});
@@ -71,7 +30,6 @@ const Layout = ({ children }) => {
 				<div>{children}</div>
 			</div>
 			<Footer />
-			<Popup {...popupData} />
 		</>
 	);
 };
